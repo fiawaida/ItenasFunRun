3707cdc2aabe32ebafc8d9542362bf1b997cd2ce
Nafia Ruwaida Chosyyatillah <52147643+fiawaida@users.noreply.github.com>
2022-07-22 11:42:39 +0700

    Add files via upload

---
+++ application/views/home/templates/header_user.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<html lang="en" class="no-js">
+
+<!-- Mirrored from demo.themeforshop.com/html_bridal/demohtml/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 15 Feb 2022 05:31:51 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="canonical" href="http://demo.themeforshop.com/" />
+	<meta name="theme-color" content="#7796A8">
+	<meta name="description" content="" />
+	<title>
+		Itenas Fun Run
+	</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+	<link href="<?= base_url() ?>assets/stylesheets/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="../../../maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/fonts.googleapis.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/icon-font.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/social-buttons.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/cs.styles.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/owl.carousel.min.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/spr.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/slideshow-fade.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/slick.css" rel="stylesheet" type="text/css" media="all">
+	<link href="<?= base_url() ?>assets/stylesheets/cs.animate.css" rel="stylesheet" type="text/css" media="all">
+
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/jquery.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/classie.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/cs.optionSelect.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/cs.script.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/cs.wishlist.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/instafeed.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/jquery.currencies.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/jquery.zoom.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/linkOptionSelectors.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/scripts.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/slick.min.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/social-buttons.js"></script>
+	<script type="text/javascript" src="<?= base_url() ?>assets/javascripts/bootstrap.min.js"></script>
+	<!-- css untuk select2 -->
+	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+	<!-- jika menggunakan bootstrap4 gunakan css ini  -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
+
+</head>
+
+<body class=" no_rtl  index-template">
+
+	<!--Header-->
+	<header id="top" class="header clearfix">
+		<div id="shopify-section-theme-header" class="shopify-section">
+			<div data-section-id="theme-header" data-section-type="header-section">
+				<section class="main-header">
+					<div class="main-header-wrapper">
+						<div class="container clearfix">
+							<div class="row">
+								<div class="main-header-inner">
+									<div class="nav-top">
+										<div class="nav-logo">
+											<div>
+												<a href="<?= base_url('home') ?>"><img src="<?= base_url() ?>assets/images/media/logo.png" alt="" title="Itenas Fun Run 50Th"></a>
+											</div>
+											<h1 style="display:none"><a href="/">FunRun</a></h1>
+										</div>
+									</div>
+									<div class="mobile-navigation">
+										<a href="<?= base_url('home/logout') ?>"><button type="button" class="btn-secondary" data-toggle="modal" data-target="#bukti">
+												LOGOUT
+											</button></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+			<script>
+				function addaffix(scr) {
+					if ($(window).innerWidth() >= 992) {
+						if (scr > 80) {
+							if (!$('#top').hasClass('affix')) {
+								$('#top').addClass('affix').addClass('fadeInDown animated');
+							}
+						} else {
+							if ($('#top').hasClass('affix')) {
+								$('#top').prev().remove();
+								$('#top').removeClass('affix').removeClass('fadeInDown animated');
+							}
+						}
+					} else $('#top').removeClass('affix');
+				}
+				$(window).scroll(function() {
+					var scrollTop = $(this).scrollTop();
+					addaffix(scrollTop);
+				});
+				$(window).resize(function() {
+					var scrollTop = $(this).scrollTop();
+					addaffix(scrollTop);
+				});
+			</script>
+		</div>
+	</header>
