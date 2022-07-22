6a5dcffbed2a2d5de23a17fcf4108b451959967e
Nafia Ruwaida Chosyyatillah <52147643+fiawaida@users.noreply.github.com>
2022-07-22 11:50:50 +0700

    Update Pembayaran.php

--- application/controllers/Pembayaran.php
+++ application/controllers/Pembayaran.php
@@ -12,11 +12,62 @@ class Pembayaran extends CI_Controller
 
     public function index()
     {
-        $data['id'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row('id_user');
-        $data['peserta'] = $this->db->get_where('peserta', ['id_user' =>  $data['id']])->row_array();
-        // die(var_dump($data['peserta']));
-        $this->load->view('home/templates/header');
-        $this->load->view('home/kehadiran', $data);
-        $this->load->view('home/templates/footer');
+        $data['peserta'] = $this->db->get_where('tbl_peserta', ['username' => $this->session->userdata('username')])->row_array();
+        if ($data['peserta']['keterangan'] == "Telah Bayar") {
+            $this->load->view('home/templates/header_user');
+            $this->load->view('home/cetak', $data);
+            $this->load->view('home/templates/footer');
+        } else if ($data['peserta']['keterangan'] == "Belum Bayar") {
+            $this->load->view('home/templates/header_user');
+            $this->load->view('home/kehadiran', $data);
+            $this->load->view('home/templates/footer');
+        } else if ($data['peserta']['keterangan'] == "Selesai") {
+            $this->load->view('home/templates/header_user');
+            $this->load->view('home/pembayaran_berhasil', $data);
+            $this->load->view('home/templates/footer');
+        }
+    }
+
+    public function upload_pembayaran()
+    {
+        $config['upload_path'] = './assets/bukti_bayar';
+        $config['encrypt_name'] = TRUE;
+        $config['allowed_types'] = 'jpeg|jpg|png';
+        $this->upload->initialize($config);
+        $upload = $this->upload->do_upload('bukti');
+        if (empty($upload)) {
+            $this->session->set_flashdata('failed', "Tambah Gagal");
+            redirect('pembayaran');
+        }
+    }
+
+    public function addPembayaran()
+    {
+        $date = date('Y-m-d H:i:s');
+        $this->upload_pembayaran();
+        $id = $this->db->get_where('tbl_peserta', ['username' => $this->session->userdata('username')])->row('id_user');
+        $tiket = $this->db->get_where('tbl_peserta', ['username' => $this->session->userdata('username')])->row('id_tiket');
+        $data = array(
+            'id_tiket' => $tiket,
+            'nama_file' => $this->input->post('nama_file'),
+            'file' => $this->upload->data('file_name'),
+            'tgl_pembayaran' => date('Y-m-d H:i:s'),
+            'id_user' => $id,
+            'created_at' => $date,
+            'updated_at' => $date
+        );
+        $query = $this->db->insert('tbl_pembayaran', $data);
+        if ($query) {
+            $data = array(
+                'keterangan' => "Telah Bayar",
+                'updated_at' => date('Y-m-d H:i:s')
+            );
+            $this->db->where('id_user', $id)->update('tbl_peserta', $data);
+            $this->session->set_flashdata('insert_akun', "Tambah Berhasil");
+        } else {
+            $this->session->set_flashdata('failed', TRUE);
+        }
+        redirect('pembayaran');
     }
 }
+
