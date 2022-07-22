d7a2b89768cfd5cbde2ee0cfc1330bfd7c611b68
Nafia Ruwaida Chosyyatillah <52147643+fiawaida@users.noreply.github.com>
2022-07-22 11:51:25 +0700

    Update Pendaftaran.php

--- application/controllers/Pendaftaran.php
+++ application/controllers/Pendaftaran.php
@@ -14,26 +14,50 @@ class Pendaftaran extends CI_Controller
 
     public function index()
     {
-        $data['kode'] = $this->PendaftaranModel->kodeM();
+        // $data['kode'] = $this->PendaftaranModel->kodeM();
         // die(var_dump($data['kode']));
+        $kode = "M-";
+        $query = "SELECT max(no_dada) as k FROM tbl_peserta";
+        $data = $this->db->query($query)->row_array();
+        $max_kode = $data['k'];
+        $max_kode2 = (int)substr($max_kode, 8, 3);
+        $max_kode2++;
+        $kode_auto = $kode . sprintf('%04s', $max_kode2);
+        $data = [
+            'no_dada' => $kode_auto
+        ];
+        // $this->db->insert('peserta', $data);
         $this->load->view('home/templates/header');
         $this->load->view('home/pendaftaran150k', $data);
         $this->load->view('home/templates/footer');
     }
-
-
+    public function kodeJ()
+    {
+        $kode = "J-";
+        $query = "SELECT max(no_dada) as k FROM tbl_peserta";
+        $data = $this->db->query($query)->row_array();
+        $max_kode = $data['k'];
+        $max_kode2 = (int)substr($max_kode, 8, 3);
+        $max_kode2++;
+        $kode_auto = $kode . sprintf('%04s', $max_kode2++);
+        $data = [
+            'no_dada' => $kode_auto
+        ];
+        return $kode_auto;
+    }
     public function tujuhlima()
     {
-        $data['kode'] = $this->PendaftaranModel->kodeJ();
+        $data['kode'] = $this->kodeJ();
         $this->load->view('home/templates/header');
         $this->load->view('home/pendaftaran75K', $data);
         $this->load->view('home/templates/footer');
     }
     public function mahasiswa()
     {
+        $data['mahasiswa'] = $this->db->get('tbl_mahasiswa')->result();
 
         $this->load->view('home/templates/header');
-        $this->load->view('home/mahasiswa');
+        $this->load->view('home/mahasiswa', $data);
         $this->load->view('home/templates/footer');
     }
     public function karyawan()
